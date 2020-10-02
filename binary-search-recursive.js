2630d42c716cd681e161a8471b10dbd42b7e7dcf
marsha <marshaannon@gmail.com>
2020-10-02 17:15:08 -0400

    final working recursive binary search

--- binary-search-recursive.js
+++ binary-search-recursive.js
@@ -2,20 +2,17 @@ function recursiveBinarySearch(array, key) {
     if (array.length === 0) {
         return false;
     }
-        
-    let midPoint = Math.floor(array.length / 2);
-    
-    if (array[midPoint] === key) {
-        return true;
-    }else {
-        if (array[midPoint] < key) {
+    else {
+        let midPoint = Math.floor(array.length / 2);
+        if (array[midPoint] === key) {
             return true;
-        }else {
-            if(array[midPoint] < key) {
-                return recursiveBinarySearch(array.slice(midPoint + 1), key);
-            } else if (list[midPoint] > key) {
-                return recursiveBinarySearch(array.slice(0,midPoint), key);
-            }
-        }
+        }else if (array[midPoint] < key) {
+            return recursiveBinarySearch(array.slice(midPoint + 1), key);
+        } else (array[midPoint] > key) 
+            return recursiveBinarySearch(array.slice(0,midPoint), key);
     }
 }
+
+
+
+    
